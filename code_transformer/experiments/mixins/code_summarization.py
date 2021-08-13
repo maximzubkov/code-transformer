1c7dfb3210149f40493810edc3f570e67aa51173
maximzubkov <zubkov.md@phystech.edu>
2021-08-13 20:52:00 +0300

    Add max seq len arg

--- code_transformer/experiments/mixins/code_summarization.py
+++ code_transformer/experiments/mixins/code_summarization.py
@@ -1,7 +1,7 @@
 from abc import ABC
 
 from code_transformer.experiments.experiment import ex, ExperimentSetup
-from code_transformer.modeling.constants import NUM_SUB_TOKENS_METHOD_NAME, NUM_SUB_TOKENS
+from code_transformer.modeling.constants import NUM_SUB_TOKENS_METHOD_NAME, NUM_SUB_TOKENS, MAX_NUM_TOKENS
 from code_transformer.preprocessing.datamanager.preprocessed import CTBufferedDataManager
 from code_transformer.preprocessing.dataset.ablation import CTCodeSummarizationOnlyASTDataset
 from code_transformer.preprocessing.dataset.code_summarization import CTCodeSummarizationDataset, \
@@ -18,7 +18,7 @@ class CTCodeSummarizationMixin(ExperimentSetup, ABC):
                    num_sub_tokens=NUM_SUB_TOKENS, num_subtokens_output=NUM_SUB_TOKENS_METHOD_NAME, use_only_ast=False,
                    use_no_punctuation=False,
                    use_pointer_network=False, sort_by_length=False, chunk_size=None, filter_language=None,
-                   dataset_imbalance=None, mask_all_tokens=False, infinite_loading=True):
+                   dataset_imbalance=None, mask_all_tokens=False, infinite_loading=True, max_num_tokens=MAX_NUM_TOKENS):
 
         if language == "poj_104":
             stage2_path = POJ_DATA_PATH_STAGE_2
@@ -59,20 +59,23 @@ class CTCodeSummarizationMixin(ExperimentSetup, ABC):
                                                                    num_sub_tokens=num_sub_tokens,
                                                                    num_sub_tokens_output=num_subtokens_output,
                                                                    use_pointer_network=use_pointer_network,
-                                                                   mask_all_tokens=mask_all_tokens)
+                                                                   mask_all_tokens=mask_all_tokens,
+                                                                   max_num_tokens=max_num_tokens)
         elif use_no_punctuation:
             self.dataset_train = CTCodeSummarizationDatasetNoPunctuation(self.data_manager,
                                                                          token_distances=token_distances,
                                                                          max_distance_mask=self.max_distance_mask,
                                                                          num_sub_tokens=num_sub_tokens,
                                                                          num_sub_tokens_output=num_subtokens_output,
-                                                                         use_pointer_network=use_pointer_network)
+                                                                         use_pointer_network=use_pointer_network,
+                                                                         max_num_tokens=max_num_tokens)
         else:
             self.dataset_train = CTCodeSummarizationDataset(self.data_manager, token_distances=token_distances,
                                                             max_distance_mask=self.max_distance_mask,
                                                             num_sub_tokens=num_sub_tokens,
                                                             num_sub_tokens_output=num_subtokens_output,
-                                                            use_pointer_network=use_pointer_network)
+                                                            use_pointer_network=use_pointer_network,
+                                                            max_num_tokens=max_num_tokens)
 
         self.use_validation = use_validation
         if self.use_validation:
@@ -91,21 +94,24 @@ class CTCodeSummarizationMixin(ExperimentSetup, ABC):
                                                                             num_sub_tokens=num_sub_tokens,
                                                                             num_sub_tokens_output=num_subtokens_output,
                                                                             use_pointer_network=use_pointer_network,
-                                                                            mask_all_tokens=mask_all_tokens)
+                                                                            mask_all_tokens=mask_all_tokens,
+                                                                            max_num_tokens=max_num_tokens)
             elif use_no_punctuation:
                 self.dataset_validation = CTCodeSummarizationDatasetNoPunctuation(data_manager_validation,
                                                                                   token_distances=token_distances,
                                                                                   max_distance_mask=self.max_distance_mask,
                                                                                   num_sub_tokens=num_sub_tokens,
                                                                                   num_sub_tokens_output=num_subtokens_output,
-                                                                                  use_pointer_network=use_pointer_network)
+                                                                                  use_pointer_network=use_pointer_network,
+                                                                                  max_num_tokens=max_num_tokens)
             else:
                 self.dataset_validation = CTCodeSummarizationDataset(data_manager_validation,
                                                                      token_distances=token_distances,
                                                                      max_distance_mask=self.max_distance_mask,
                                                                      num_sub_tokens=num_sub_tokens,
                                                                      num_sub_tokens_output=num_subtokens_output,
-                                                                     use_pointer_network=use_pointer_network)
+                                                                     use_pointer_network=use_pointer_network,
+                                                                     max_num_tokens=max_num_tokens)
 
         self.dataset_validation_creator = \
             lambda infinite_loading: self._create_validation_dataset(stage2_path,
@@ -119,11 +125,12 @@ class CTCodeSummarizationMixin(ExperimentSetup, ABC):
                                                                      use_pointer_network,
                                                                      filter_language,
                                                                      dataset_imbalance,
-                                                                     mask_all_tokens)
+                                                                     mask_all_tokens,
+                                                                     max_num_tokens)
 
     def _create_validation_dataset(self, data_location, language, use_only_ast, use_no_punctuation, token_distances,
                                    num_sub_tokens, num_subtokens_output, infinite_loading, use_pointer_network,
-                                   filter_language, dataset_imbalance, mask_all_tokens):
+                                   filter_language, dataset_imbalance, mask_all_tokens, max_num_tokens):
         data_manager_validation = CTBufferedDataManager(data_location, language, partition="valid",
                                                         shuffle=True, infinite_loading=infinite_loading,
                                                         filter_language=filter_language,
@@ -135,20 +142,23 @@ class CTCodeSummarizationMixin(ExperimentSetup, ABC):
                                                                    num_sub_tokens=num_sub_tokens,
                                                                    num_sub_tokens_output=num_subtokens_output,
                                                                    use_pointer_network=use_pointer_network,
-                                                                   mask_all_tokens=mask_all_tokens)
+                                                                   mask_all_tokens=mask_all_tokens,
+                                                                   max_num_tokens=max_num_tokens)
         elif use_no_punctuation:
             dataset_validation = CTCodeSummarizationDatasetNoPunctuation(data_manager_validation,
                                                                          token_distances=token_distances,
                                                                          max_distance_mask=self.max_distance_mask,
                                                                          num_sub_tokens=num_sub_tokens,
                                                                          num_sub_tokens_output=num_subtokens_output,
-                                                                         use_pointer_network=use_pointer_network)
+                                                                         use_pointer_network=use_pointer_network,
+                                                                         max_num_tokens=max_num_tokens)
         else:
             dataset_validation = CTCodeSummarizationDataset(data_manager_validation,
                                                             token_distances=token_distances,
                                                             max_distance_mask=self.max_distance_mask,
                                                             num_sub_tokens=num_sub_tokens,
                                                             num_sub_tokens_output=num_subtokens_output,
-                                                            use_pointer_network=use_pointer_network)
+                                                            use_pointer_network=use_pointer_network,
+                                                            max_num_tokens=max_num_tokens)
 
         return dataset_validation
